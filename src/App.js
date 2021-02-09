0844e50a7da13645828df76467f49a9117d89704
Madlen Engel <engel.madlen@googlemail.com>
2021-02-09 15:36:31 +0100

    Add ContextProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import EquipmentContextProvider from "./context/EquipmentContext";
+import EmployeeContextProvider from "./context/EmployeeContext";
+import RoomContextProvider from "./context/RoomContext";
 import Home from "./components/Home";
 import Equipment from "./components/Equipment";
 import Employees from "./components/Employees";
@@ -16,9 +18,9 @@ function App() {
       <Switch>
         <Route exact path="/"><Home /></Route>
         <Route exact path="/equipment"><EquipmentContextProvider><Equipment /></EquipmentContextProvider></Route>
-        <Route exact path="/employees"><Employees /></Route>
+        <Route exact path="/employees"><EmployeeContextProvider><Employees /></EmployeeContextProvider></Route>
         <Route exact path="/employees/:id"></Route>
-        <Route exact path="/rooms"><Rooms /></Route>
+        <Route exact path="/rooms"><RoomContextProvider><Rooms /></RoomContextProvider></Route>
         <Route exact path="/rooms/:id"></Route>
       </Switch>
     </div>
